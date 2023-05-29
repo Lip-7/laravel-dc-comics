63e9f84676dff3a200c32d6b89ef99869adf4e3f
Lip-7 <zhighofficial@gmail.com>
2023-05-29 17:06:32 +0200

    created store and create for comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,9 +3,10 @@
     @include('partials.jumbo')
     <div class="wrapper bg-dark">
         <section class="container">
-            <h2 class="text-primary fw-bold pt-2">All our Comics</h2>
-            <div class="d-flex justify-content-end">
-                <button class="btn btn-primary ">Add new Comic</button>
+            <h2 class="text-primary fw-bold pt-3">All our Comics</h2>
+            <div class="d-flex justify-content-end mb-1">
+                <a href="{{route('comics.create')}}"><button class="btn btn-primary ">Add new Comic</button></a>
+
             </div>
             <div class="cardsContainer p-5 d-flex flex-wrap gap-4 justify-content-center">
                 @foreach ($comics as $card)

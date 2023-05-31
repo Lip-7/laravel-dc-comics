1dd6325b6a81648ccd7808e78d3f9ff7422dae23
Lip-7 <zhighofficial@gmail.com>
2023-05-31 18:09:22 +0200

    minor fixes

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,13 +2,12 @@
 @section('content')
     @include('partials.jumbo')
     <div class="wrapper bg-dark">
-        <section class="container pt-3">
+        <section class="container pt-5">
             @if (session()->has('message'))
                 <div class="alert alert-success ToDisappear">
                     {{ session()->get('message') }}
                 </div>
             @endif
-            <h2 class="text-primary fw-bold">All our Comics</h2>
             <div class="d-flex justify-content-end mb-1">
                 <a href="{{route('comics.create')}}"><button class="btn btn-primary ">Add new Comic</button></a>
 
